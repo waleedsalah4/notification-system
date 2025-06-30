762a5e141cc773e5ef588ff6729550783338f2aa
waleedsalah4 <walidsalah652@gmail.com>
2025-06-30 15:10:28 +0300

    update main title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     <main className="relative container flex min-h-screen flex-col items-center justify-center gap-10">
       <header className="flex flex-col items-center gap-4">
         <h1>Notification Toast</h1>
-        <p className="text-lg text-gray-400">A toast component for React.</p>
+        <p className="text-lg text-gray-400">A customizable toast notification system for React and Angular with built-in animations, positioning, auto-dismiss, and more.</p>
         <div className="flex items-center gap-4">
           <ShowToastButton />
           <GithubButton />
